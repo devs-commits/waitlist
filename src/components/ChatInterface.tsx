554a699eea4a4725a5e8472d2b12de0f4d2e526c
Lloydgapton <lloydgapton123@gmail.com>
2026-03-13 07:37:14 +0000

    removed unused variable

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -137,7 +137,7 @@ const ChatInterface = ({ variant = 'v3' }: ChatInterfaceProps) => {
 
           <div className="h-64 overflow-y-auto p-4 space-y-4 bg-white">
             <AnimatePresence>
-              {messages.map((message, index) => (
+              {messages.map((message) => (
                 <motion.div
                   key={message.id}
                   initial={{ opacity: 0, y: 10 }}
